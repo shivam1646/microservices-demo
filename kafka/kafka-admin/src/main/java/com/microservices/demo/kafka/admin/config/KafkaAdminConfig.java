d8e6fa2f6deabd91f10ce0dc6d45ec4fb2890427
shivam1646 <shivamkotak16.sk@gmail.com>
2021-06-27 21:41:24 +0530

    Kafka to Elasticsearch service implementation

--- kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/KafkaAdminConfig.java
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/KafkaAdminConfig.java
@@ -6,12 +6,14 @@ import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 @EnableRetry
 @Configuration
 public class KafkaAdminConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminConfig.class);
 
     private final KafkaConfigData kafkaConfigData;
 
@@ -22,6 +24,8 @@ public class KafkaAdminConfig {
     @Bean
     public AdminClient adminClient() {
         Properties properties = new Properties();
+        LOG.info("CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG {}", CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG);
+        LOG.info("kafkaConfigData.getBootstrapServers {}", kafkaConfigData.getTopicName());
         properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers());
         return AdminClient.create(properties);
     }
